f784aecfb4ce2028e5ce3eeead9c1371b814604f
Blaben <blabensolutions@gmail.com>
2025-07-20 20:37:26 +0000

    del

--- django-models/LibraryProject/relationship_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Author, Book, UserProfile, library
-
-# Register your models here.
-
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(UserProfile)
-admin.site.register(library)
