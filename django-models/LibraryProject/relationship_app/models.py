06baeeb5691ad622f26b90fd6c3d846c6dc0cd16
Blaben <blabensolutions@gmail.com>
2025-07-20 19:31:17 +0000

    user profile admin

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -39,8 +39,14 @@ class Librarian(models.Model):
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=10, choices=[('admin', 'Admin'), ('librarian', 'Librarian'), ('member', 'Member')])
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    )
 
-    # def __str__(self):
-    #     return self.user
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
