b1584922e345f657c6e482421595ab711aa04c9f
Blaben <blabensolutions@gmail.com>
2025-08-10 22:49:30 +0000

    modifications of the "self", "APITestCase

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,6 +1,65 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
 from .models import Book
+from django.contrib.auth.models import User
 
-class BookTestCase(TestCase):
-    pass
 
+class BookAPITestCase(APITestCase):
+
+    def setUp(self):
+        # Create test user for authentication
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.client.login(username='testuser', password='testpass')
+
+        # Create a sample book
+        self.book = Book.objects.create(
+            title="Test Book",
+            author="Test Author",
+            published_year=2024
+        )
+
+        # Define URLs 
+        self.list_url = reverse('book-list')
+        self.detail_url = reverse('book-detail', kwargs={'pk': self.book.pk})
+
+    def test_list_books(self):
+        """Test retrieving the list of books"""
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('title', response.json()[0])
+
+    def test_get_single_book(self):
+        """Test retrieving a single book"""
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()['title'], self.book.title)
+
+    def test_create_book(self):
+        """Test creating a new book"""
+        data = {
+            "title": "New Book",
+            "author": "New Author",
+            "published_year": 2025
+        }
+        response = self.client.post(self.list_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 2)
+
+    def test_update_book(self):
+        """Test updating an existing book"""
+        data = {
+            "title": "Updated Book",
+            "author": "Updated Author",
+            "published_year": 2026
+        }
+        response = self.client.put(self.detail_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book.refresh_from_db()
+        self.assertEqual(self.book.title, "Updated Book")
+
+    def test_delete_book(self):
+        """Test deleting a book"""
+        response = self.client.delete(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 0)
