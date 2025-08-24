d73773adcd6f4a352ca9b93b4a587ad48601509f
Blaben <blabensolutions@gmail.com>
2025-08-24 22:27:40 +0000

    rest fram auth imported

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import CustomUser
+from rest_framework.authtoken.models import Token
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = '__all__'
+        
